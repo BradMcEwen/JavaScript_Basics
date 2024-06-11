2de035ba793110ccfe23c113ce292c7703260f4f
BradMcEwen <mcewen.brad30@gmail.com>
2024-06-10 23:23:49 -0400

    Java Banks

---
+++ homework.js
@@ -0,0 +1,43 @@
+let myStudent = "Brad ";
+console.log(myStudent)
+let myAge = 26 ;
+console.log(myAge)
+let myGrade = " A ";
+console.log(myGrade)
+let myAttendance = "Present ";
+console.log(myAttendance)
+
+let myData = myStudent + myAge + myGrade + myAttendance;
+
+console.log(myData);
+
+const x = 4;
+const y = 2;
+const sum = x + y;
+const diff = x - y;
+const product = x * y;
+const quot = x / y;
+
+console.log(sum);
+console.log(diff);
+console.log(product);
+console.log(Math.floor(quot));
+
+let myNum = 8
+myNum += 5
+myNum -= 1
+
+console.log(myNum);
+
+const myVal = 4
+const myStringVal = "3"
+
+console.log(myVal > myStringVal);
+console.log(myVal == myStringVal);
+console.log(myVal !== myStringVal);
+
+
+let myOtherNum = 0;
+
+console.log(myNum > myOtherNum && myOtherNum < myStringVal);
+console.log(myNum % 2 == 0 || myStringVal % 2 == 0);
